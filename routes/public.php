f1f21b5e3c54b8adf801ce0cdb825bf73fe206c8
Salman Kabir <sksshouvo2@gmail.com>
2024-02-05 11:15:32 +0600

    Working on Authorization

---
+++ routes/public.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Authentication\UserController;
+/*
+|--------------------------------------------------------------------------
+| Public API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api/public" middleware group. Make something great!
+|
+*/
+
+Route::group(['prefix' => 'authenticaion'], function() {
+    Route::post('register', [UserController::class, 'register'])->name('user.register');
+});
