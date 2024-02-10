6c55b0e7d91fb2a95028cfff4d152f4a8153525c
Salman Kabir <sksshouvo2@gmail.com>
2024-02-11 02:09:21 +0600

    Woking on rss feed

---
+++ app/Http/Controllers/RssFeed/RssFeedController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\RssFeed;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\RssFeed;
+
+class RssFeedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return "index";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return "Show";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return "update";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return "destroy";
+    }
+}
